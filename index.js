461cf4c7856a1bf3144b7b516c08239bd9f05926
Tanvir Anzum <shihabahmed133075@gmail.com>
2022-03-24 23:31:47 +0600

    a

--- index.js
+++ index.js
@@ -168,6 +168,16 @@ async function run() {
              res.json(result)
            })
            
+             app.get('/users/:email', async (req, res) => {
+               const email = req.params.email
+               const query = { email: email }
+               const user = await usersCollection.findOne(query)
+               let isAdmin = false
+               if (user?.role === 'admin') {
+                 isAdmin = true
+               }
+               res.json({ admin: isAdmin })
+             })
     app.get('/singleItem/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: ObjectId(req.params.id) }
